fd7c1c8e365c5a317f28229832d1e9f3541c4e7f
mathuranc1 <70967644+mathuranc1@users.noreply.github.com>
2025-07-16 23:15:38 -0400

    Fixed minor issues with code

--- lab04/part1/javascript/main.js
+++ lab04/part1/javascript/main.js
@@ -16,7 +16,7 @@ function randomValueFromArray(array){
 // initialize string input variables
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:," +
                 "they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was" + 
-                "not surprised — :insertx: weighs 300 pounds, and it was a hot DynamicsCompressorNode.";
+                "not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 let insertY = ["the soup kitchen", "Disneyland", "the White House"];
 let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
@@ -25,7 +25,8 @@ let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk"
 randomize.addEventListener('click', result);
 
 /**
- * 
+ * Generates randomized story for user
+ * @returns {void}
  */
 function result() {
   // initialize String variables
@@ -35,25 +36,25 @@ function result() {
   let zItem = randomValueFromArray(insertZ);
 
   // replace placeholders in newStory
-  newStory = newStory.replace(":insertx:", xItem);
-  newStory = newStory.replace(":inserty:", yItem);
-  newStory = newStory.replace(":insertz:", zItem);
+  newStory = newStory.replaceAll(":insertx:", xItem);
+  newStory = newStory.replaceAll(":inserty:", yItem);
+  newStory = newStory.replaceAll(":insertz:", zItem);
 
   // replaces 'Bob' w/ user's custom name
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace("Bob", name);
+    newStory = newStory.replaceAll("Bob", name);
   }
 
   // converts imperial units to metric
   if(document.getElementById("uk").checked) {
     // lbs to st
     const weight = Math.round(300/14) + ' stone';
-    newStory = newStory.replace("94 fahrenheit", weight);
+    newStory = newStory.replaceAll("94 fahrenheit", weight);
 
     // °F to °C                  
     const temperature =  Math.round((94-32)*(5/9)) + ' centigrade';
-    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replaceAll("300 pounds", temperature);
   }
 
   // output randomly generated story for user
