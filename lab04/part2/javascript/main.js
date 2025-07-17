d33a435c12ae839e26c2d628ab46734c54dc86b9
mathuranc1 <70967644+mathuranc1@users.noreply.github.com>
2025-07-17 00:11:52 -0400

    declared array of image file names

--- lab04/part2/javascript/main.js
+++ lab04/part2/javascript/main.js
@@ -1,3 +1,4 @@
+// initialize global variables
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,6 +6,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFileNames = [
+    "pic1.jpg", 
+    "pic2.jpg",
+    "pic3.jpg",          
+    "pic4.jpg", 
+    "pic5.jpg"
+]
 
 /* Declaring the alternative text for each image file */
 
