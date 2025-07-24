57ef49c890745a84b22ba736c125b34004f41d11
mathuranc1 <70967644+mathuranc1@users.noreply.github.com>
2025-07-24 12:07:00 -0400

    Create class for Ball

--- lab04/part3/js/main.js
+++ lab04/part3/js/main.js
@@ -1,18 +1,27 @@
-// setup canvas
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+}
 
+// setup canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
+// generates random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
+// generates random color
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
