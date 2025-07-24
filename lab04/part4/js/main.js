606dd7c0806201725dee92ab04d50e8e1eb32645
mathuranc1 <70967644+mathuranc1@users.noreply.github.com>
2025-07-24 14:30:21 -0400

    Create event listener for evil circle

--- lab04/part4/js/main.js
+++ lab04/part4/js/main.js
@@ -114,3 +114,21 @@ function loop() {
 }
 
 loop();
+
+// lets user control evil circle via WASD
+window.addEventListener("keydown", e => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
