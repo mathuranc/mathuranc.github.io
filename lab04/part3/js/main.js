3fa2d937b43631d83075794821bef5783d42b1bc
mathuranc1 <70967644+mathuranc1@users.noreply.github.com>
2025-07-24 14:24:32 -0400

    Changed min value for velY

--- lab04/part3/js/main.js
+++ lab04/part3/js/main.js
@@ -95,7 +95,7 @@ while (balls.length < 25) {
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
-    random(7, 7),
+    random(-7, 7),
     randomRGB(),
     size,
   );
